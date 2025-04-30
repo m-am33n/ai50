4a900a770c0bde648fa9f3ccbba955342bfcc2aa
Ameen Yoosuf <ameen.yoosuf@exinity.com>
2025-04-30 22:34:49 +0400

    some more tweaks

--- neuralnetworks/traffic/traffic.py
+++ neuralnetworks/traffic/traffic.py
@@ -86,12 +86,20 @@ def get_model():
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
         tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
         tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dense(NUM_CATEGORIES, activation='softmax')
     ])
+
     model.compile(
         optimizer='adam',
         loss='categorical_crossentropy',
         metrics=['accuracy'])
+
     return model
+
+if __name__ == "__main__":
+    main()
